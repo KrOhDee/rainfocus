a0f0e71198d7d80a1b12a172458af18370661175
Luis Aleman <lalem014@fiu.edu>
2023-03-03 18:09:56 -0500

    Responsive styling changes

--- src/components/DashboardInner.js
+++ src/components/DashboardInner.js
@@ -4,19 +4,23 @@ import AttendeesDropdown from './AttendeesDropdown';
 export default function DashboardInner() {
   return (
     <div className="inner--main">
-      <p className="inner--title">RainFocus Summit</p>
-      <p className="inner--location">Lehi, UT * December 15th</p>
-      <input className="inner--input" placeholder="Search"></input>
-      <div className="inner--selections">
-        <InnerSelections name="Guide" />
-        <InnerSelections
-          classNameCircle="inner--attendee-circle"
-          classNameBg="inner--attendee-bg"
-          name="Attendees"
-        />
-        <AttendeesDropdown />
-        <InnerSelections name="Content" />
-        <InnerSelections name="Exhibitor" />
+      <div>
+        <p className="inner--title">RainFocus Summit</p>
+        <p className="inner--location">Lehi, UT * December 15th</p>
+        <input className="inner--input" placeholder="Search"></input>
+        <div className="inner--selections">
+          <InnerSelections name="Guide" />
+
+          <InnerSelections
+            classNameCircle="inner--attendee-circle"
+            classNameBg="inner--attendee-bg"
+            name="Attendees"
+          />
+          <AttendeesDropdown />
+
+          <InnerSelections name="Content" />
+          <InnerSelections name="Exhibitor" />
+        </div>
       </div>
     </div>
   );
